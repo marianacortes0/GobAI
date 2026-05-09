b025ade0b76513f34a30dbf8b859d63f09503273
marianacortes0 <marigenerosita@gmail.com>
2026-05-09 12:27:37 -0500

    a

---
+++ src/types/cytoscape-plugins.d.ts
@@ -0,0 +1,9 @@
+declare module 'cytoscape-cose-bilkent' {
+  const coseBilkent: any;
+  export default coseBilkent;
+}
+
+declare module 'cytoscape-navigator' {
+  const navigator: any;
+  export default navigator;
+}
